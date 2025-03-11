b4d8f61b994fafdef84f3fbbaa953b1b328c6a51
Nick Young <nick.young@auckland.ac.nz>
2025-03-11 13:01:01 +1300

    bake city names and timezones into the JSON

---
+++ geocode.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+from glob import glob
+import json
+from pprint import pprint
+import requests
+import requests_cache
+from shapely import Point
+requests_cache.install_cache('cache')
+from timezonefinder import TimezoneFinder
+from tqdm.auto import tqdm
+import re
+import pandas as pd
+import geopandas as gpd
+tf = TimezoneFinder()
+
+dfs = []
+for url in ["https://ingress.com/news/2024-sharedmem", "https://ingress.com/news/2024-erasedmem", "https://ingress.com/news/2025-plusalpha"]:
+  r = requests.get(url)
+  df = pd.DataFrame(re.findall(r"(?P<lat>-?\d+.\d+), (?P<lng>-?\d+.\d+)]\).bindPopup\('Shard Skirmish<br /> ?(?P<city>.+?)<br />(?P<date>.+?)'", r.text), columns=["lat", "lng", "city", "date"])
+  df["series"] = url.split("/")[-1]
+  print(df)
+  dfs.append(df)
+df = pd.concat(dfs)
+df["lat"] = df["lat"].astype(float)
+df["lng"] = df["lng"].astype(float)
+df = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df["lng"], df["lat"]), crs="EPSG:4326")
+print(df)
+df.drop(columns="geometry").to_csv("events.csv", index=False)
+
+files = glob("*.json")
+
+for f in tqdm(files):
+  with open(f, "r") as file:
+    print(f)
+    data = json.load(file)
+    filtered_data = [d for d in data["artifact"] if d.get("fragment")]
+    for d in tqdm(filtered_data):
+      samplePortal = d["fragment"][0]["history"][-1]["destinationPortalInfo"]
+      lat = samplePortal["latE6"] / 1e6
+      lng = samplePortal["lngE6"] / 1e6
+      df["distance"] = df.distance(Point(lng, lat))
+      df = df.sort_values("distance")
+      city = df.iloc[0]["city"]
+      timezone = tf.timezone_at(lng=lng, lat=lat)
+      print(d["name"], city, timezone)
+      d["city"] = city
+      d["timezone"] = timezone
+  with open(f, "w") as file:
+    json.dump(data, file, indent=2, ensure_ascii=False)
