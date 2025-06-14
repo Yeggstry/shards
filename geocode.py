d76f2bfe3344df27f23ae032c7bd03e7b6416b2a
Nick Young <nick.young@auckland.ac.nz>
2025-06-14 22:50:52 +1200

    Perth

--- geocode.py
+++ geocode.py
@@ -18,16 +18,17 @@ tf = TimezoneFinder()
 dfs = []
 for url in ["https://ingress.com/news/2024-sharedmem", "https://ingress.com/news/2024-erasedmem", "https://ingress.com/news/2025-plusalpha", "https://ingress.com/news/2025-plustheta"]:
   r = requests.get(url)
-  df = pd.DataFrame(re.findall(r"(?P<lat>-?\d+.\d+), (?P<lng>-?\d+.\d+)]\).bindPopup\('Shard Skirmish<br /> ?(?P<city>.+?)<br />(?P<date>.+?)'", r.text), columns=["lat", "lng", "city", "date"])
+  df = pd.DataFrame(re.findall(r"(?P<lat>-?\d+.\d+), (?P<lng>-?\d+.\d+)]\).bindPopup\('(?P<type>Shard Skirmish|Anomaly)<br /> ?(?P<city>.+?)<br />(?P<date>.+?)'", r.text), columns=["lat", "lng", "type", "city", "date"])
   df["series"] = url.split("/")[-1]
   print(df)
   dfs.append(df)
 df = pd.concat(dfs)
 df["lat"] = df["lat"].astype(float)
 df["lng"] = df["lng"].astype(float)
+df["date"] = pd.to_datetime(df["date"])
 df = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df["lng"], df["lat"]), crs="EPSG:4326")
 print(df)
-df.drop(columns="geometry").to_csv("events.csv", index=False)
+df.drop(columns="geometry").sort_values("date").to_csv("events.csv", index=False)
 
 files = glob("*.json")
 
